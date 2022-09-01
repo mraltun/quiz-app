0a8e875d3b512c5abb137835fffdbf57019a7763
Murat Altun <mraltun@gmail.com>
2022-09-01 16:37:34 +0300

    Added stats to context

--- src/context/quizContext.js
+++ src/context/quizContext.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { useState, useContext, useEffect } from "react";
 
 const table = {
+  film: 11,
+  television: 14,
   sports: 21,
   history: 23,
   politics: 24,
@@ -9,12 +11,35 @@ const table = {
 
 const API_ENDPOINT = "https://opentdb.com/api.php?";
 
-const url = "";
+const tempUrl = "https://opentdb.com/api.php?amount=10&category=14";
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>;
+  const [waiting, setWaiting] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [error, setError] = useState(false);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <AppContext.Provider
+      value={{
+        waiting,
+        loading,
+        questions,
+        index,
+        correct,
+        error,
+        isModalOpen,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 export const useGlobalContext = () => {
   return useContext(AppContext);
