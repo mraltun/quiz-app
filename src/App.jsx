68babb47d13f0efd526a24d0961c7599a685c970
Murat Altun <mraltun@gmail.com>
2022-09-02 13:09:20 +0300

    Added states to App and render logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { useGlobalContext } from "./context/quizContext";
 import Loading from "./components/Loading";
 import Modal from "./components/Modal";
 import SetupForm from "./components/SetupForm";
 
 const App = () => {
-  return <div>App</div>;
+  const { waiting, loading, questions, index, correct } = useGlobalContext();
+
+  // Show the form if we are waiting for user input
+  if (waiting) {
+    return <SetupForm />;
+  }
+
+  // Show loading if we are still fetching the data
+  if (loading) {
+    return <Loading />;
+  }
+
+  return <main>quiz app</main>;
 };
 
 export default App;
